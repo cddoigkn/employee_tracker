d350a22f769bf101a0c5945a4c6275aaaa7bc194
Casey Doig <cddoigkn@gmail.com>
2023-11-20 23:15:38 -0600

    here's some more info

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,20 @@
 
 
 -- don't need to actually put in id value because of auto_increment --
-INSERT INTO department (id, name)
-VALUES (""),
-  (""),
-  ("");
+INSERT INTO department (name)
+VALUES ("Sales"),
+  ("Corporate"),
+  ("Law");
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES ("", "", "45000.00", ""),
-  ("", "", "70000.00", ""),
-  ("", "", "130000.00", ""),
-  ("", "", "180000.00", ""),
+INSERT INTO role (title, salary, department_id)
+VALUES ("Service Clerk", "45000.00", 0),
+  ("Clerk Manager", "70000.00", 1),
+  ("Store Manager", "130000.00", 1),
+  ("Lawyer", "180000.00", 2);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES ("", "", "")
+VALUES ("James", "Bimdopulus", 0, 1),
+ ("Bjorg", "Minestrone", 1, 2),
+ ("Waffle", "Pojankumus", 2),
+ ("John", "Smith", 3);
+
--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-///
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: 'kingofall',
+    database: 'employee_tracker_db'
+  },
+  console.log(`Connected to the employee_tracker database.`)
+);
